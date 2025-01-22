3ac898ec6cbea013491bea1b0f366b4951eed398
red-star25 <nakumdhruv@yahoo.com>
2025-01-21 23:50:22 -0800

    - Add address and Token gen func implemented

--- controllers/address.go
+++ controllers/address.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -9,15 +10,142 @@ import (
 	"github.com/red-star25/advance-go/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// import "github.com/gin-gonic/gin"
+func AddAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userID := ctx.Query("id")
 
-// func AddAddress() gin.HandlerFunc{}
+		if userID == "" {
+			ctx.Header("Content-Type", "application/json")
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Invalid code"})
+			ctx.Abort()
+			return
+		}
 
-// func EditAddress() gin.HandlerFunc{}
+		address, err := primitive.ObjectIDFromHex(userID)
+		if err != nil {
+			ctx.IndentedJSON(500, "internal server error")
+		}
 
-// func EditWorkAddress() gin.HandlerFunc{}
+		var addresses models.Address
+
+		addresses.AdressID = primitive.NewObjectID()
+
+		if err := ctx.BindJSON(&addresses); err != nil {
+			ctx.IndentedJSON(http.StatusNotAcceptable, err.Error())
+		}
+
+		var c, cancel = context.WithTimeout(context.Background(), time.Second*100)
+		defer cancel()
+
+		matchFilter := bson.D{{Key: "$match", Value: bson.D{primitive.E{Key: "_id", Value: address}}}}
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$address"}}}}
+		group := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: "$address_id"}, {Key: "count", Value: bson.D{primitive.E{Key: "$sum", Value: 1}}}}}}
+
+		pointCursor, err := UserCollection.Aggregate(c, mongo.Pipeline{matchFilter, unwind, group})
+		if err != nil {
+			ctx.IndentedJSON(500, "internal server error")
+		}
+
+		var addressInfo []bson.M
+		if err = pointCursor.All(c, &addressInfo); err != nil {
+			panic(err)
+		}
+
+		var size int32
+		for _, address_no := range addressInfo {
+			count := address_no["count"]
+			size += count.(int32)
+		}
+		if size < 2 {
+			filter := bson.D{primitive.E{Key: "_id", Value: address}}
+			update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
+			_, err := UserCollection.UpdateOne(c, filter, update)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			ctx.IndentedJSON(400, "not allowed")
+		}
+		defer cancel()
+		ctx.Done()
+	}
+}
+
+func EditHomeAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userID := ctx.Query("id")
+
+		if userID == "" {
+			ctx.Header("Content-Type", "application/json")
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Invalid"})
+			ctx.Abort()
+			return
+		}
+
+		user_id, err := primitive.ObjectIDFromHex(userID)
+		if err != nil {
+			ctx.IndentedJSON(500, "internal server error")
+		}
+		var editAddress models.Address
+		if err := ctx.BindJSON(&editAddress); err != nil {
+			ctx.IndentedJSON(http.StatusBadRequest, err.Error())
+		}
+		var c, cancel = context.WithTimeout(context.Background(), time.Second*100)
+		defer cancel()
+
+		filter := bson.D{primitive.E{Key: "_id", Value: user_id}}
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address.0.house_name", Value: editAddress.House}, {Key: "address.0.street_name", Value: editAddress.Street}, {Key: "address.0.city_name", Value: editAddress.City}, {Key: "address.0.pin_code", Value: editAddress.Pincode}}}}
+
+		_, err = UserCollection.UpdateOne(c, filter, update)
+		if err != nil {
+			ctx.IndentedJSON(500, "something went wrong")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		ctx.IndentedJSON(200, "Successfully updated Home address")
+	}
+}
+
+func EditWorkAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userID := ctx.Query("id")
+
+		if userID == "" {
+			ctx.Header("Content-Type", "application/json")
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Invalid"})
+			ctx.Abort()
+			return
+		}
+
+		user_id, err := primitive.ObjectIDFromHex(userID)
+		if err != nil {
+			ctx.IndentedJSON(500, "internal server error")
+		}
+		var editAddress models.Address
+		if err := ctx.BindJSON(&editAddress); err != nil {
+			ctx.IndentedJSON(http.StatusBadRequest, err.Error())
+		}
+		var c, cancel = context.WithTimeout(context.Background(), time.Second*100)
+		defer cancel()
+
+		filter := bson.D{primitive.E{Key: "_id", Value: user_id}}
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address.1.house_name", Value: editAddress.House}, {Key: "address.1.street_name", Value: editAddress.Street}, {Key: "address.1.city_name", Value: editAddress.City}, {Key: "address.1.pin_code", Value: editAddress.Pincode}}}}
+
+		_, err = UserCollection.UpdateOne(c, filter, update)
+		if err != nil {
+			ctx.IndentedJSON(500, "something went wrong")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		ctx.IndentedJSON(200, "Successfully updated Home address")
+
+	}
+}
 
 func DeleteAddress() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
--- tokens/token_gen.go
+++ tokens/token_gen.go
@@ -1,1 +1,61 @@
 package tokens
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/v4"
+	jwt "github.com/dgrijalva/jwt-go/v4"
+	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/red-star25/advance-go/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var UserData *mongo.Collection = database.UserData(database.Client, "Users  ")
+var SECRET_KEY = os.Getenv("SECRET_KEY")
+
+type SignedDetails struct {
+	Email     string
+	FirstName string
+	LastName  string
+	uID       string
+	jwt.StandardClaims
+}
+
+func TokenGenerator(email string, firstName string, lastName string, uID string) (signedToken string, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		uID:       uID,
+		StandardClaims: jwt.StandClamis{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken
+}
+
+func ValidateToken() {}
+
+func UpdateAllToken() {}
